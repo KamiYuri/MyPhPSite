67db80eb90aa027142de6c3f52aa50728365b4c3
Hoàng Việt Dũng <dung.hv194254@sis.hust.edu.vn>
2022-05-14 11:38:18 +0700

    init commit

--- connect.php
+++ connect.php
@@ -3,12 +3,12 @@
     $username = "gideon";
     $password = "2801";
     $dbname = "blog";
+    $port = 3306;
 
     // Create connection
-    $conn = new mysqli($servername, $username, $password, $dbname);
+    $conn = new mysqli($servername, $username, $password, $dbname, $port);
 
     // Check connection
     if ($conn->connect_error) {
         die("Connection failed: " . $conn->connect_error);
     }
-?>
